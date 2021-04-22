9f6986721d63a4990a497b698cac771ca60c1597
sunwoo0326 <78770195+sunwoo0326@users.noreply.github.com>
2021-04-22 17:35:55 +0900

    Add files via upload

--- main.java
+++ main.java
@@ -1,34 +1,18 @@
-class CardTest{
-    public static void main(String args[]) {
-        System.out.println("Card.width= "+ Card.width);
-        System.out.println("Card.height= "+ Card.height);
+calss Data (int x;)
 
-        Card c1 = new Card();
-        c1.kind = "Heart";
-        c1.number = 7;
+class ReferenceReturnEx {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
 
-        Card c2 = new Card();
-        c2.kind = "Spade";
-        c2.number = 4;
-
-        System.out.println("c1은 " + c1.kind + ", " + c1.number
-        + "이며, 크기는 (" + c2. width+ ", " +c2 number")");
-        System.out.println("c2는 " + c2.kind + ", " + c2.number
-        + "이며, 크기는(" + c2.width + ", " +c2.height+ ")");
-        System.out.println("c1의 width와 height를 각각 50, 80 으로 변경합니다.");
-        c1.width = 50;
-        c1.height = 80;
-
-        System.out.println("c1은"+ c1.kind +", "+ c1.number
-        +"이며,크기는 ("+ c1.width+","+c1.height+")");
-        System.out.println("c2는" + c2.kind + ", "+ c2.number
-        +"이며, 크기는 ("+ c2.width + ", " + c2.height +")");
-
+        Data d2 = copy(d);
+        System.out.println("d.x ="+ d.x);
+        System.out.println("d2.x="+d2.x);
     }
-}
-class Card{
-    String kind;
-    int number;
-    static int width = 100;
-    static int height = 250;
+    static Data copy(Data d) {
+        Data tmp = new Data();
+        tmp.x = d.x;
+        
+        return tmp;
+    }
 }
