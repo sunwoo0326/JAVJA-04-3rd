dbaf00d24d631ee2614960a18f7ce9e59aafbc4e
sunwoo0326 <78770195+sunwoo0326@users.noreply.github.com>
2021-04-23 17:57:02 +0900

    Add files via upload

--- main.java
+++ main.java
@@ -1,18 +1,31 @@
-calss Data (int x;)
+Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+    
+    String months[] = {  
+      "January", "February", "March", "April", 
+      "May","June","July","August",
+      "September","October","November","December"
+  };
+    int re = 0;
 
-class ReferenceReturnEx {
-    public static void main(String[] args) {
-        Data d = new Data();
-        d.x = 10;
+    System.out.println("해당 월의 영어 단어를 입력하시오");
+    System.out.println("첫 글자는 대문자, 나머지는 소문자로 입력하세요.");
+    
+    do{
+    int month = 1 + rand.nextInt(12);
+  
+    while(true){
+    System.out.print(month + "월:");
+    String s = sc.next();
 
-        Data d2 = copy(d);
-        System.out.println("d.x ="+ d.x);
-        System.out.println("d2.x="+d2.x);
-    }
-    static Data copy(Data d) {
-        Data tmp = new Data();
-        tmp.x = d.x;
-        
-        return tmp;
+    if (s.equals(months[month-1])) break; 
+      System.out.println("틀렸습니다.");
+    }  
+      System.out.print("정답입니다.");
+      System.out.print("다시 한번? 1....Yes / 0....No: ");
+      re =  sc.nextInt();
+  
+    }while(re == 1);
+
     }
 }
